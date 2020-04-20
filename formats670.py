06a80a84c4fa4a4bb35f4d29b59eaf8b5b8f6858
kakawkaization <kakawkaization@gmail.com>
2020-04-20 22:49:19 +0300

    670 need to write programm, write write __name__ instruction

--- formats670.py
+++ formats670.py
@@ -6,3 +6,11 @@ def commas(N):
         digits, last3 = digits[:-3], digits[-3:]
         result = (last3 + ',' + result) if result else last3
     return result
+
+def money(N, width=0):
+    sign = '-' if N < 0 else ''
+    N = abs(N)
+    whole = commas(int(N))
+    fract = ('%.2f' % N)[-2:]
+    format = '%s%s.%s' % (sign, whole, fract)
+    return '$%*s' % (width, format)
