2eb625b2a6f9261100bf898ea069d710c885c0bf
kakawkaization <kakawkaization@gmail.com>
2020-05-13 22:29:37 +0300

    675, need to complete mydir675.py part 2

--- mydir675.py
+++ mydir675.py
@@ -7,3 +7,17 @@ seplen = 60
 sepchr = '-'
 
 def listing(module, verbose=True):
+    sepline = sepchr * seplen
+    if verbose:
+        print(sepline)
+        print('name:', module.__name__, 'file:', module.__file__)
+        print(sepline)
+
+    count = 0
+    for attr in module.__dict__:
+        print("%02d) %s" % (count, attr), end=' ')
+        if attr.startswith('__'):
+            print("built-in name")
+        else:
+            print(getattr(module, attr))
+            count = count + 1
