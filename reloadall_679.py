e030e96e6ea899864a2b44112b293729e35abeda
kakawkaization <kakawkaization@gmail.com>
2020-06-10 23:21:55 +0300

    679, need to finish reload679.py

--- reloadall_679.py
+++ reloadall_679.py
@@ -7,3 +7,12 @@ from imp import reload
 
 def status(module):
     print('reloading' + module.__name__)
+
+def transitive_reload(module, visited):
+    if not module visited:
+        status(module)
+        reload(module)
+        visited[module] = None
+        for attrobj in module.__dict__.values():
+            if type(attrobj) == types.ModuleType:
+                transitive_reload(attrobj, visited)
