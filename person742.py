0efc6c1228c5249b09cdd7521b52b355252ea496
SimpleBSD <admin@simplebsd.ru>
2020-09-29 21:58:10 +0300

    743

--- person742.py
+++ person742.py
@@ -1,11 +1,28 @@
 class Person:
-    def __init__(self, job=None, pay=0):
+    def __init__(self, name, job=None, pay=0):
         self.name = name
         self.job = job
         self.pay = pay
     def lastName(self):
         return self.name.split()[-1]
     def giveRaise(self, percent):
-        self.pay = int(self.pay * (1 + percent)
+        self.pay = int(self.pay * (1 + percent))
     def __str__(self):
-        return '[Person: %s, %s]' %(self.name, self.pay)
+        return '[Person: %s, %s]' % (self.name, self.pay)
+
+class Manager(Person):
+    def giveRaise(self, percent, bonus=.10):
+        Person.giveRaise(self, percent + bonus)
+
+if __name__ == '__main__':
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job='dev', pay=100000)
+    print(bob)
+    print(sue)
+    print(bob.lastName(), sue.lastName())
+    sue.giveRaise(.10)
+    print(sue)
+    tom = Manager('Tom Jones', 'mgr', 50000)
+    tom.giveRaise(.10)
+    print(tom.lastName())
+    print(tom)
