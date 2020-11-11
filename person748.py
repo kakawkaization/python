bfc2bd56e3d362d474558e7a04d5e85a52d98d3f
SimpleBSD <admin@simplebsd.ru>
2020-11-11 15:38:16 +0300

    file ,748 need to read

--- person748.py
+++ person748.py
@@ -12,10 +12,13 @@ class Person:
 
 class Manager(Person):
     def __init__(self, name, pay):
-        Person.__init__(self, name, 'mgr', pay)
-
+        self.person = Person(name, 'mgr', pay)
     def giveRaise(self, percent, bonus=.10):
-        Person.giveRaise(self, percent + bonus)
+        self.person.giveRaise(percent + bonus)
+    def __getattr__(self, attr):
+        return getattr(self.person, attr)
+    def __str__(self):
+        return str(self.person)
 
 if __name__ == '__main__':
     bob = Person('Bob Smith')
