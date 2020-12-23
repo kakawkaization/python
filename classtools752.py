cb18c785f2fb8f2b6afe468b840c53a162baee53
SimpleBSD <admin@simplebsd.ru>
2020-12-23 22:03:15 +0300

    753

--- classtools752.py
+++ classtools752.py
@@ -7,3 +7,18 @@ class AttrDisplay:
 
     def __str__(self):
         return '[%s: %s]' % (self.__class__.__name__, self.gatherAttrs())
+
+if __name__ == '__main__':
+    class TopTest(AttrDisplay):
+        count = 0
+        def __init__(self):
+            self.attr1 = TopTest.count
+            self.attr2 = TopTest.count + 1
+            TopTest.count += 2
+    
+    class SubTest(TopTest):
+        pass
+
+    X, Y = TopTest(), SubTest()
+    print(X)
+    print(Y)
