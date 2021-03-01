cf039c2c544b457a154c0f202ad8938f1806864c
SimpleBSD <admin@simplebsd.ru>
2021-03-01 23:31:28 +0300

    805, need to finish script

---
+++ skipiterator805.py
@@ -0,0 +1,11 @@
+class SkipIterator:
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+        self.offset = 0
+    def next(self):
+        if self.offset >= len(self.wrapped):
+            raise StopIteration
+        else:
+            item = self.wrapped[self.offset]
+            self.offset += 2
+            return item
