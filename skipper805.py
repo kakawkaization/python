9e4da937fbaff31fe39c299e188e8dd75cc1f265
SimpleBSD <admin@simplebsd.ru>
2021-03-09 23:36:09 +0300

    805, iter() returned non-iterator of type SkipIterator

--- skipper805.py
+++ skipper805.py
@@ -4,24 +4,8 @@ class SkipIterator:
         self.offset = 0
     def next(self):
         if self.offset >= len(self.wrapped):
-            raise StopItrration
+            raise StopIteration
         else:
             item = self.wrapped[self.offset]
             self.offset += 2
             return item
-
-class SkipObject:
-    def __init__(self, wrapped):
-        self.wrapped = wrapped
-    def __iter__(self):
-        return SkipIterator(self.wrapped)
-
-if __name__ == '__main__':
-    alpha = 'abcdef'
-    skipper = SkipObject(alpha)
-    I = iter(skipper)
-    print(next(I), next(I), next(I))
-
-    for x in skipper:
-        for y in skipper:
-            print(x + y, end=' ')
